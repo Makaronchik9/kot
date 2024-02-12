05d2213da0049d72c4abc5c9f733595b694a3112
user <89169555954@mail.ru>
2024-02-13 00:07:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+//        System.out.println("hello world");
+//        System.out.println(10);
+//        System.out.println("hello"+"world");
+//        System.out.println("hello"+" "+"world");
+        System.out.println("a + b = "+(2+3));
+        System.out.println("a + b = "+2+3);
+        System.out.println("(a + b)*2 = "+ (2+3)*5);
+    }
+}
